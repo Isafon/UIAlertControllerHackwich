98d3529c10b8ed7f164098f2cd8b0acd23596a73
Isa Fontana <isfon8@gmail.com>
2022-03-28 13:32:22 -0600

    finished almost

--- UIAlertControllerHackwich/ViewController.swift
+++ UIAlertControllerHackwich/ViewController.swift
@@ -9,7 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var labelToChangeFromAlert: UILabel!
+    
+    
+
+    @IBAction func onAddButtonTapped(_ sender: Any) {
+        
+//ALERT:
+        let alert = UIAlertController(title: "What is Your Name?", message: nil, preferredStyle: .alert)
+        
+        self.present(alert, animated: true, completion: nil)
+        
+        
+        let ok = UIAlertAction(title: "OK", style: .default){ action in
+            print("OK Pressed")
+        }
+        alert.addAction(ok)
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+        alert.addAction(cancel)
+        
+//TEXT FIELDS:
+        alert.addTextField(configurationHandler: {textField in
+            textField.placeholder = "First Name"
+        })
+        alert.addTextField(configurationHandler: {textField in
+            textField.placeholder = "Last Name"
+        })
+        
+        let ok2 = UIAlertAction(title: "OK", style: .default){ action in
+            print(alert.textFields?.first)
+            print(alert.textFields?.last)
+//LABEL CHANGE:
+            let ok3 = UIAlertAction(title: "OK", style: .default){ [self] action in
+                if let firstName = alert.textFields![0].text,
+                   let lastName = alert.textFields![1].text {
+                    
+                    self.labelToChangeFromAlert.text = ("Name: " + firstName + "" + lastName)
+                }
+                
+            }
+    }
+    
+        func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
@@ -17,3 +59,4 @@ class ViewController: UIViewController {
 
 }
 
+}
